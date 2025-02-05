d49e2101789fe5153320f39012da7bcf9aa3c9e8
Yutosaki <sasayu8190@gmail.com>
2025-02-05 22:49:35 +0900

    feat: add feature

--- setting.sh
+++ setting.sh
@@ -1,1 +1,3 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/Aloxaf/fzf-tab
