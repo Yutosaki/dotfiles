4a6f1f55d9110c3384b50013c2a5266323d285f9
Yutosaki <sasayu8190@gmail.com>
2025-02-06 15:34:04 +0900

    feat: add fzf

--- setting.sh
+++ setting.sh
@@ -1,3 +1,19 @@
+#!/bin/sh
+
+# fzf インストールスクリプト
+if [ "$(uname)" = "Linux" ]; then
+    sudo apt-get update
+    sudo apt-get install fzf
+elif [ "$(uname)" = "Darwin" ]; then
+    brew update
+    brew install fzf
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+
+# 必要なリポジトリをクローン
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/Aloxaf/fzf-tab
+
