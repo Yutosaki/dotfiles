10b69ecdbe597406b8fe4fea9da77180600e0800
Yutosaki <sasayu8190@gmail.com>
2025-02-20 12:02:46 +0900

    feat: unset relative number

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,18 +1,20 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
 -- Set up custom filetypes
+vim.opt.relativenumber = false -- 相対行番号を無効にする
+
 vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
+  pattern = {        -- 先にパターンを評価
     ["~/%.config/foo/.*"] = "fooscript",
   },
+  filename = {       -- 次にファイル名を評価
+    ["Foofile"] = "fooscript",
+  },
+  extension = {      -- 最後に拡張子を評価
+    foo = "fooscript",
+  },
 }
+
