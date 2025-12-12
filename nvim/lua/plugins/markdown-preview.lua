f569791a07f2d838808439444bfd1e7e9faf0cc1
Yutosaki <sasayu8190@gmail.com>
2025-12-12 15:04:28 +0900

    feat: markdown preview plugin

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+  -- Optional: Add a keybinding as suggested previously
+  keys = {
+    { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview" },
+  },
+}
