d3f6c2b16a4f702c248919283860cdd7db3d4482
Yutosaki <sasayu8190@gmail.com>
2025-12-17 19:18:14 +0900

    feat: add to install ripgrep

--- setting.sh
+++ setting.sh
@@ -19,7 +19,7 @@ if [ "$(uname)" = "Linux" ]; then
         sudo apt-get remove -y fzf
     fi
 
-    sudo apt-get install -y lsd bat build-essential unzip valgrind
+    sudo apt-get install -y lsd bat build-essential unzip valgrind ripgrep
     
     if command -v batcat &> /dev/null && ! command -v bat &> /dev/null; then
         mkdir -p ~/.local/bin
@@ -32,7 +32,7 @@ elif [ "$(uname)" = "Darwin" ]; then
         exit 1
     fi
     brew update
-    brew install lsd bat valgrind
+    brew install lsd bat valgrind ripgrep
 else
     echo "❌ Unsupported OS"
     exit 1
