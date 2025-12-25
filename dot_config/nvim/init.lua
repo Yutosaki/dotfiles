8beeafec9b856ee2e60cb3f41a83e88fcfd74bbe
Yutosaki <sasayu8190@gmail.com>
2025-12-26 00:56:14 +0900

    feat: add clip board setting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,21 +4,25 @@ local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
 vim.opt.display:append("lastline")
 vim.g.neovide_font_name = "Hack Nerd Font Mono"
+
 -- validate that lazy is available
 if not pcall(require, "lazy") then
   -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.api.nvim_echo(
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = "*",
   callback = function()
     vim.opt.display = "lastline"
@@ -26,5 +30,37 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
   end
 })
 
+-- -------------------------------------------------------------------------
+-- クリップボード設定 (OSC 52 対応)
+-- Docker / SSH 環境でもホストOSのクリップボードと同期します
+-- -------------------------------------------------------------------------
+if vim.ui.clipboard and vim.ui.clipboard.osc52 then
+  -- Neovim 0.10+ の標準機能を使用
+  local function copy(lines, _)
+    require('vim.ui.clipboard.osc52').copy('+')(lines)
+  end
+
+  local function paste()
+    return require('vim.ui.clipboard.osc52').paste('+')()
+  end
+
+  vim.g.clipboard = {
+    name = 'osc52',
+    copy = {
+      ['+'] = copy,
+      ['*'] = copy,
+    },
+    paste = {
+      ['+'] = paste,
+      ['*'] = paste,
+    },
+  }
+  -- 常にシステムクリップボードを使う設定
+  vim.opt.clipboard = "unnamedplus"
+end
+
+-- -------------------------------------------------------------------------
+-- 設定読み込み (ここがファイルの最後に来るようにする)
+-- -------------------------------------------------------------------------
 require "lazy_setup"
 require "polish"
