f8be540c8e23cbc5e9310ca42bb3134c9ba48c51
Yutosaki <sasayu8190@gmail.com>
2025-12-26 22:31:26 +0900

    feat: add clipboard setting for docker

---
+++ dot_config/nvim/lua/plugins/clipboard.lua
@@ -0,0 +1,24 @@
+-- DocekrやSSHを使用する時にクリップボードを使用できるように
+return {
+  -- クリップボード設定の強制上書き
+  {
+    "nvim-lua/plenary.nvim", -- 読み込みタイミング調整のためのダミー
+    init = function()
+      -- 1. クリップボードプロバイダを OSC 52 に固定する
+      vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+          ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+          ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+          ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+          ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+        },
+      }
+
+      -- 2. "unnamedplus" オプションで常にシステムクリップボードを使う
+      vim.opt.clipboard = "unnamedplus"
+    end,
+  },
+}
