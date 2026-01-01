e6eb96ffee51fcceb4fe92b4644e34ec1ec0ce0c
Yutosaki <sasayu8190@gmail.com>
2026-01-01 21:07:45 +0900

    feat: change color when search

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -69,4 +69,10 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("astrocore").setup(opts)
+
+    -- ここで色を強制指定（シアン背景、黒文字、太字）
+    vim.api.nvim_set_hl(0, "Search", { bg = "#00ffff", fg = "#000000", bold = true })
+  end,
 }
