3cdcb4c7c9f57051c8f314348f5920a907b24cd5
Yutosaki <sasayu8190@gmail.com>
2026-01-04 00:09:56 +0900

    feat: add back space keymap

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,16 +1,16 @@
 return {
-    "stevearc/oil.nvim",
-    opts = {},
-    -- 依存関係（アイコン表示のため）
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("oil").setup({
-            -- ここに詳細な設定を書けます
-            view_optionw = {
-                show_hidden = true, -- 隠しファイルを表示
-            },
-        })
-        -- oilを開くためのキーバインド（例: - キーで親ディレクトリを開く）
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end,
+	"stevearc/oil.nvim",
+	opts = {},
+	-- 依存関係（アイコン表示のため）
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			-- ここに詳細な設定を書けます
+			view_optionw = {
+				show_hidden = true, -- 隠しファイルを表示
+			},
+		})
+		-- oilを開くためのキーバインド（例: - キーで親ディレクトリを開く）
+		vim.keymap.set("n", "<BS>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
 }
