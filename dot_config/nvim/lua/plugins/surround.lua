135908c45c1f830af7e74b752114c08601097f78
Yutosaki <sasayu8190@gmail.com>
2026-01-03 22:03:35 +0900

    feat: easily edit surround

---
+++ dot_config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+return {
+  "kylechui/nvim-surround",
+  version = "*", -- 最新の安定版を使用
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- デフォルトの設定を使用（必要であればここでカスタマイズ可能）
+    })
+  end,
+}
