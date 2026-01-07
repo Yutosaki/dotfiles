ee5e89a825133e6d33e13a2ac1654d1b4a131305
Yutosaki <sasayu8190@gmail.com>
2026-01-07 14:21:12 +0900

    feat: delete polish.lua and set jj to exit toggle term

--- dot_config/nvim/lua/mapping.lua
+++ dot_config/nvim/lua/mapping.lua
@@ -1,47 +1,121 @@
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
+-- C言語用の定義ジャンプ関数
+local function jump_to_c_implementation()
+	local word = vim.fn.expand("<cword>") -- カーソル下の単語
+	local src_dir = "src"
+	local found = false
+	local workspace_root = vim.fn.getcwd()
+	local src_path = workspace_root .. "/" .. src_dir
 
--- jj でインサートモードを抜ける
-map("i", "jj", "<ESC>", opts)
+	if vim.fn.isdirectory(src_path) == 1 then
+		local files = vim.fn.glob(src_path .. "/*.c", false, true)
+		for _, file in ipairs(files) do
+			local content = table.concat(vim.fn.readfile(file), "\n")
+			-- 関数定義パターン検索
+			local pattern = "[%w%*]+%s+" .. vim.fn.escape(word, "^$()%.[]*+-?") .. "%s*%("
+			local start_pos = content:find(pattern)
 
------------------------------------------
--- 1. 画面移動を爆速にする
------------------------------------------
--- Ctrl + hjkl でウィンドウ間を移動できるようにします
--- (デフォルトだと Ctrl+w を押してから h とか押さないといけないのが面倒なので)
-map("n", "<C-h>", "<C-w>h", opts) -- 左のウィンドウへ
-map("n", "<C-j>", "<C-w>j", opts) -- 下のウィンドウへ
-map("n", "<C-k>", "<C-w>k", opts) -- 上のウィンドウへ
-map("n", "<C-l>", "<C-w>l", opts) -- 右のウィンドウへ
+			if start_pos then
+				vim.cmd("edit " .. file)
+				local line_num = 1
+				local pos = 1
+				while pos < start_pos do
+					pos = content:find("\n", pos) + 1
+					if not pos then
+						break
+					end
+					line_num = line_num + 1
+				end
+				vim.api.nvim_win_set_cursor(0, { line_num, 0 })
+				found = true
+				break
+			end
+		end
+	end
 
------------------------------------------
--- 2. インデント操作の改善 (Visual Mode)
------------------------------------------
--- 範囲選択して > でインデントすると、選択範囲が解除されてしまうのを防ぎます。
--- 連続で > > > と押せるようになります。
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+	if not found then
+		vim.lsp.buf.definition()
+	end
+end
 
------------------------------------------
--- 3. x で文字を消した時にヤンク（コピー）しない
------------------------------------------
--- "x" で一文字消すと、それがクリップボードに入ってしまい、
--- ペーストしようとしてたものが上書きされる事故を防ぎます。
-map("n", "x", '"_x', opts)
+return {
+	-----------------------------------------------------------------------------
+	-- Normal Mode (n)
+	-----------------------------------------------------------------------------
+	n = {
+		-- 1. バッファ移動 (Shift + h/l)
+		["L"] = {
+			function()
+				require("astrocore.buffer").nav(vim.v.count1)
+			end,
+			desc = "Next buffer",
+		},
+		["H"] = {
+			function()
+				require("astrocore.buffer").nav(-vim.v.count1)
+			end,
+			desc = "Previous buffer",
+		},
+		["C"] = {
+			function()
+				require("astrocore.buffer").close()
+			end,
+			desc = "Close buffer safely",
+		},
 
------------------------------------------
--- 5. 行の移動 (Visual Mode)
------------------------------------------
--- 選択した行を J や K で上下に移動させます（VSCodeの Alt+Up/Down のような動き）
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '<-2<CR>gv=gv", opts)
+		-- 3. 画面分割 (Ctrl + j のあとに \ や - )
+		["<C-j>\\"] = { ":vsplit<CR>", desc = "Vertical Split" },
+		["<C-j>-"] = { ":split<CR>", desc = "Horizontal Split" },
 
--- 検索系 (Telescope)
--- Space + ff : ファイル名で検索
-map("n", "<Leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
--- Space + fw : ファイル内の文字で検索 (Live Grep)
-map("n", "<Leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Find words" })
--- Space + fb : 開いているバッファを検索
-map("n", "<Leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
--- Space + fh : ヘルプドキュメントを検索
-map("n", "<Leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Find help" })
+		-- 4. 画面移動 (Ctrl + hjkl)
+		["<C-h>"] = { "<C-w>h", desc = "Move to left window" },
+		["<C-j>"] = { "<C-w>j", desc = "Move to bottom window" },
+		["<C-k>"] = { "<C-w>k", desc = "Move to top window" },
+		["<C-l>"] = { "<C-w>l", desc = "Move to right window" },
+
+		-- 5. 行の移動
+		["J"] = { ":m .+1<CR>==", desc = "Move line down" },
+		["K"] = { ":m .-2<CR>==", desc = "Move line up" },
+
+		-- 6. 特殊機能: C言語実装へジャンプ (gi)
+		["gi"] = { jump_to_c_implementation, desc = "Go to C implementation" },
+
+		-- 7. その他
+		["x"] = { '"_x', desc = "Delete character without yank" },
+		["<leader>ff"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" },
+		["<leader>fw"] = { "<cmd>Telescope live_grep<cr>", desc = "Find words" },
+		["<leader>fb"] = { "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+		["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", desc = "Find help" },
+		["<Leader>t"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+	},
+
+	-----------------------------------------------------------------------------
+	-- Visual Mode (v)
+	-----------------------------------------------------------------------------
+	v = {
+		["J"] = { ":m '>+1<CR>gv=gv", desc = "Move selection down" },
+		["K"] = { ":m '<-2<CR>gv=gv", desc = "Move selection up" },
+		["<"] = { "<gv", desc = "Unindent line" },
+		[">"] = { ">gv", desc = "Indent line" },
+	},
+
+	-----------------------------------------------------------------------------
+	-- Visual Block Mode (x)
+	-----------------------------------------------------------------------------
+	x = {
+		["p"] = { '"_dP', desc = "Paste without yanking" },
+	},
+
+	-----------------------------------------------------------------------------
+	-- Insert Mode (i)
+	-----------------------------------------------------------------------------
+	i = {
+		["jj"] = { "<Esc>", desc = "Escape insert mode" },
+	},
+
+	-----------------------------------------------------------------------------
+	-- Terminal Mode (t)
+	-----------------------------------------------------------------------------
+	t = {
+		["jj"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+	},
+}
--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -1,87 +1,44 @@
--- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
--- Configuration documentation can be found with `:h astrocore`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
 	"AstroNvim/astrocore",
-	---@type AstroCoreOpts
-	opts = {
-		-- Configure core features of AstroNvim
-		features = {
-			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-			autopairs = true, -- enable autopairs at start
-			cmp = true, -- enable completion at start
-			diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-			highlighturl = true, -- highlight URLs at start
-			notifications = true, -- enable notifications at start
-		},
-		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-		diagnostics = {
+	opts = function(_, opts)
+		local my_maps = require("mapping")
+		opts.mappings = require("astrocore").extend_tbl(opts.mappings, my_maps)
+
+		opts.features = {
+			large_buf = { size = 1024 * 256, lines = 10000 },
+			autopairs = true,
+			cmp = true,
+			diagnostics_mode = 3,
+			highlighturl = true,
+			notifications = true,
+		}
+
+		opts.diagnostics = {
 			virtual_text = true,
 			underline = true,
-		},
-		-- vim options can be configured here
-		options = {
-			opt = { -- vim.opt.<key>
-				relativenumber = true, -- sets vim.opt.relativenumber
-				number = true, -- sets vim.opt.number
-				spell = false, -- sets vim.opt.spell
-				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-				wrap = false, -- sets vim.opt.wrap
-			},
-			g = { -- vim.g.<key>
-				-- configure global vim variables (vim.g)
-				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-				-- This can be found in the `lua/lazy_setup.lua` file
-			},
-		},
-		-- Mappings can be configured through AstroCore as well.
-		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-		mappings = {
-			-- first key is the mode
-			n = {
-				-- second key is the lefthand side of the map
+		}
 
-				-- navigate buffer tabs
-				["]b"] = {
-					function()
-						require("astrocore.buffer").nav(vim.v.count1)
-					end,
-					desc = "Next buffer",
-				},
-				["[b"] = {
-					function()
-						require("astrocore.buffer").nav(-vim.v.count1)
-					end,
-					desc = "Previous buffer",
-				},
+		opts.options = {
+			opt = {
+				relativenumber = false,
+				number = true,
+				showmode = true,
+				laststatus = 2,
+				wrap = true,
+				linebreak = true,
+				display = "lastline",
 
-				-- mappings seen under group name "Buffer"
-				["<Leader>bd"] = {
-					function()
-						require("astroui.status.heirline").buffer_picker(function(bufnr)
-							require("astrocore.buffer").close(bufnr)
-						end)
-					end,
-					desc = "Close buffer from tabline",
-				},
-				["<Leader>t"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
-				-- tables with just a `desc` key will be registered with which-key if it's installed
-				-- this is useful for naming menus
-				-- ["<Leader>b"] = { desc = "Buffers" },
-
-				-- setting a mapping to false will disable it
-				-- ["<C-S>"] = false,
+				spell = false,
+				signcolumn = "yes",
 			},
-		},
-	},
+		}
+
+		return opts
+	end,
+
 	config = function(_, opts)
 		require("astrocore").setup(opts)
-		require("mapping")
-
-		-- ここで色を強制指定（シアン背景、黒文字、太字）
 		vim.api.nvim_set_hl(0, "Search", { bg = "#00ffff", fg = "#000000", bold = true })
 	end,
 }
