b00fd862220efc807ec38256ce525a727334f147
Yutosaki <sasayu8190@gmail.com>
2026-02-05 00:14:06 +0100

    refactor: apply format

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,9 +3,9 @@
 local lazypath = vim.env.LAZY or vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
+	-- stylua: ignore
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+		lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -14,10 +14,10 @@ vim.g.neovide_font_name = "Hack Nerd Font Mono"
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
-  -- stylua: ignore
-  vim.api.nvim_echo(
-    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
-    true, {})
+	-- stylua: ignore
+	vim.api.nvim_echo(
+		{ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+		true, {})
 	vim.fn.getchar()
 	vim.cmd.quit()
 end
